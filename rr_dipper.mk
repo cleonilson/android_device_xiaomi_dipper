6d65ce372047fb47d07f691f0b359e25836250b5
Cleonilson Vieira <cleonilson.vieira@gmail.com>
2020-10-21 18:33:52 -0300

    dipper: changes to RR rom

--- rr_dipper.mk
+++ rr_dipper.mk
@@ -7,14 +7,17 @@
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_dipper
+PRODUCT_NAME := rr_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
 PRODUCT_MANUFACTURER := Xiaomi
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys"
 
